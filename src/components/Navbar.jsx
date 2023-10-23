535fcc957de88c4b67b9e70680e242554460af51
darq <prototypid@gmail.com>
2023-10-23 19:01:15 +0530

    Add loggedin user options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 
 export default function Navbar() {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -18,6 +19,12 @@ export default function Navbar() {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "darq x",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -33,8 +40,27 @@ export default function Navbar() {
           <span>Explore</span>
           <span>English</span>
           <span>Sign In</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img src="/vite.svg" alt="" />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       {active && (
