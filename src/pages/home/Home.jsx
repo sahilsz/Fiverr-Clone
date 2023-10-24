aa1cd7cf83e28f9336ccb6bb770c5c94195c547a
darq <prototypid@gmail.com>
2023-10-24 15:33:20 +0530

    Add second feature section on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,11 @@ import Slide from "../../components/Slide/Slide";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import { cards } from "../../data";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5} className="w-100">
@@ -14,6 +15,82 @@ export default function Home() {
           <CategoryCard key={card.id} item={card} />
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The Best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The Best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The Best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The Best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/vid.mp4" controls></video>
+          </div>
+        </div>
+      </div>
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>
+              fiverr <i>business</i>
+            </h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits,
+              dedicated to businesses
+            </p>
+            <div className="title">
+              <img src="./img/check.png" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" />
+              Get matched with the perfect talent by a customer sucess manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explore fiverr business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/51c35c7cecf75e6a5a0110d27909a2f5-1690202609364/EN.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
