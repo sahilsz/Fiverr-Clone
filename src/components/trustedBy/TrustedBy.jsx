9e06d58ab853ec694c1bddaae3ad474fb057e6f4
darq <prototypid@gmail.com>
2023-10-24 14:03:09 +0530

    Add TrustedBy component on the home page

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,31 @@
+import "./TrustedBy.css";
+
+export default function TrustedBy() {
+  return (
+    <div className="trusted">
+      <div className="container">
+        <span>Trusted By:</span>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta2x.b364aec.png"
+          alt="Meta"
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/google2x.4fa6c20.png"
+          alt="Google"
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/netflix2x.6b36ad6.png"
+          alt="Netflix"
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/pandg2x.0d06f7b.png"
+          alt="P&G"
+        />
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/paypal2x.d2fa54d.png"
+          alt="PayPal"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 export default function Home() {
   return (
     <div>
       <Featured />
+      <TrustedBy />
     </div>
   );
 }
