ae74aaec4d5467ca8821c3a525f24368605072c5
darq <prototypid@gmail.com>
2023-10-25 09:38:47 +0530

    Add Gigs page design

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import GigCard from "../../components/gigCard/GigCard";
+import "./Gigs.css";
+
 export default function Gigs() {
-  return <div>Gigs</div>;
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">FIVERR &gt; GRAPHICS & DESIGN</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverrs's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort By</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
