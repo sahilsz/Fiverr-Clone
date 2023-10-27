71e17e24b66bc8e13f288b7835987b2d5305320d
darq <prototypid@gmail.com>
2023-10-27 10:50:09 +0530

    Add new gig add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,3 +1,73 @@
+import "./Add.css";
+
 export default function Add() {
-  return <div>Add</div>;
+  return (
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g. I will do something I'm realling good at"
+            />
+
+            <label htmlFor="cat">Category</label>
+            <select name="" id="cat">
+              <option value="design">Design</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple />
+
+            <label htmlFor="">Description</label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="16"
+              placeholder="Brief descriptions to introduce your service to customers"
+            ></textarea>
+
+            <button>Create</button>
+          </div>
+          <div className="right">
+            <label htmlFor="">Service Title</label>
+            <input type="text" placeholder="e.g. One-page web design" />
+
+            <label htmlFor="">Short Description</label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Short description of your service"
+            ></textarea>
+
+            <label htmlFor="">Delivery Time (e.g. 3 days)</label>
+            <input type="number" min={1} />
+
+            <label htmlFor="">Revision Number</label>
+            <input type="number" min={1} />
+
+            <label htmlFor="">Add Features</label>
+            <input type="text" placeholder="e.g. page desing" />
+            <input type="text" placeholder="e.g. file uploading" />
+            <input type="text" placeholder="e.g. setting up a domain" />
+            <input type="text" placeholder="e.g. hosting" />
+
+            <label htmlFor="">Price</label>
+            <input type="number" min={1} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
