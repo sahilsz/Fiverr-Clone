932427d73c0868f1915c0d820f1c3c6c76886e24
darq <prototypid@gmail.com>
2023-11-14 17:18:42 +0530

    feat: Add button to go to chat page

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,15 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import "./Orders.css";
 import req from "../../utils/newRequest.js";
+import { useNavigate } from "react-router-dom";
 
 export default function Orders() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const navigate = useNavigate();
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () => req("/orders").then((res) => res.data),
   });
 
+  const handleContact = async (order) => {
+    const sellerId = order.sellerId;
+    const buyerId = order.buyerId;
+    const id = sellerId + buyerId;
+
+    try {
+      const res = await req(`/c/${id}`);
+      navigate(`/message/${res.data.id}`);
+    } catch (err) {
+      if (err.response.status === 404) {
+        const res = await req.post("/c", {
+          to: currentUser.isSeller ? buyerId : sellerId,
+        });
+
+        navigate(`/message/${res.data.id}`);
+      }
+    }
+  };
   return (
     <div className="orders">
       {isLoading ? (
@@ -40,7 +61,12 @@ export default function Orders() {
                   <td>{order.title}</td>
                   <td>{order.price}</td>
                   <td>
-                    <img className="message" src="./img/message.png" alt="" />
+                    <img
+                      className="message"
+                      src="./img/message.png"
+                      alt=""
+                      onClick={() => handleContact(order)}
+                    />
                   </td>
                 </tr>
               ))}
